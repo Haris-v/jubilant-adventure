db81fae0753cdd08b80214c713625632bcdef3c4
Haris-v <harveraros@gmail.com>
2022-05-05 19:43:37 +0300

    newGit

---
+++ src/javaapplication552022/JavaApplication552022.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication552022;
+
+/**
+ *
+ * @author Haris
+ */
+public class JavaApplication552022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                System.out.println("Hello world");
+    }
+    
+}
